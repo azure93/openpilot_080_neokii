f77241c8a3d0772450d8362f2f19dcef2e6a1f9d
azure93 <azure93@gmail.com>
2020-12-05 15:47:00 +0900

    steering disable when turn on blinker

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -229,7 +229,7 @@ class CarInterface(CarInterfaceBase):
       ret.cruiseState.enabled = ret.cruiseState.available
 
     # turning indicator alert logic
-    if (ret.leftBlinker or ret.rightBlinker or self.CC.turning_signal_timer) and ret.vEgo < LANE_CHANGE_SPEED_MIN - 1.2:
+    if ret.leftBlinker or ret.rightBlinker or self.CC.turning_signal_timer:
       self.CC.turning_indicator_alert = True
     else:
       self.CC.turning_indicator_alert = False
